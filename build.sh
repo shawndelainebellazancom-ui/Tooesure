e8c16648231e5722381ffcf25fa351f2fb685fb6
Shawn Delaine Bellazan <shawndelainebellazancom@gmail.com>
2025-12-25 20:45:41 -0600

    update

--- build.sh
+++ build.sh
@@ -29,6 +29,6 @@ echo "Restoring packages..."
 dotnet restore "$PROJECT_FILE" --configfile nuget.config
 
 echo "Publishing..."
-dotnet publish "$PROJECT_FILE" -c Release -o ./output
+dotnet publish "$PROJECT_FILE" -c Release -o ./output /p:UseAppHost=false
 
-echo "Build complete."  
+echo "Build complete."
